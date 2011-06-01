26aa61b85b9dd3758c20a6e47330a3d5a4fe0ad2
Christoph Haag <haagch.christoph@googlemail.com>
2011-06-01 18:04:06 +0200

    initial commit

---
+++ comments.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# (internal) routine to decode urlencoded strings
+
+echo "Content-type: text/html"
+echo
+echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">'
+echo "<HTML><HEAD>"
+echo "<TITLE>Comments</TITLE>"
+echo '<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />'
+echo "</HEAD>"
+echo "<body><h1>Comments</h1>"
+echo '<div><u>Write comment (all fields optional)</u>:
+<br><br><form action="writecomments.sh" method="GET">
+   Nickname: <input type="text" name="nickname"><br>
+   eepsite: <input type="text" name="eepsite"><br><br>
+   Text:<br>
+   <textarea rows=10 cols=50 name="msg"></textarea><br>
+   <input type="submit" value="Submit!">
+</form></div>'
+echo "<br><br>"
+echo "<h3>Former Comments</h3>"
+tac comments.txt | while read I
+do
+   NICK=$(echo "$I" | cut -f 1 -d "|")
+   DATE=$(date --date="@$(echo $I | cut -f 2 -d "|")" +"%T %D")
+   WWW=$(echo $(echo $I | cut -f 3 -d "|") | grep -Eo "http://[a-z0-9/-_\.]*") #regex could be improved
+   MSG=$(echo "$I" | cut --complement -f 1-3 -d "|")
+   echo '<div style="border:solid 1px; width:50em; padding: 1em">'
+   echo "<p>$NICK said at $DATE</p><hr>"
+   echo "<div>$MSG</div>"
+   if [ -n "$WWW" ]
+   then
+      echo "<a href=\"$WWW\">$WWW</a>"
+   fi
+   echo "</div><br>"
+done
+echo "</body></html>"
---
+++ writecomments.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+function cgi_decodevar()
+{
+   [ $# -ne 1 ] && return
+   local v t h
+   # replace all + with whitespace and append %%
+   t="${1//+/ }%%"
+   while [ ${#t} -gt 0 -a "${t}" != "%" ];
+   do
+      v="${v}${t%%\%*}" # digest up to the first %
+      t="${t#*%}" # remove digested part
+      # decode if there is anything to decode and if not at end of string
+      if [ ${#t} -gt 0 -a "${t}" != "%" ]
+         then h=${t:0:2} # save first two chars
+         t="${t:2}" # remove these
+         v="${v}"`echo -e \\\\x${h}` # convert hex to special char
+      fi
+   done
+   # return decoded string
+   echo "${v}"
+   return
+}
+
+echo "Content-type: text/html"
+echo
+echo "<HTML><HEAD>"
+echo "<TITLE>Comment written</TITLE></HEAD>"
+echo "<body><h1>Comment hopefully written</h1>"
+
+NICKU=$(echo "$QUERY_STRING" | sed -n 's/^.*nickname=\([^&]*\).*$/\1/p' | sed "s/%20/ /g" | sed "s/+/ /g")
+MSGU=$(echo "$QUERY_STRING" | sed -n 's/^.*msg=\([^&]*\).*$/\1/p' | sed "s/%20/ /g" | sed "s/+/ /g")
+WWWU=$(echo "$QUERY_STRING" | sed -n 's/^.*WWW=\([^&]*\).*$/\1/p' | sed "s/%20/ /g" | sed "s/+/ /g")
+
+NICK=$(cgi_decodevar "$NICKU" | recode -pf ..html)
+MSG=$(cgi_decodevar "$MSGU" | recode -pf ..html)
+WWW=$(cgi_decodevar "$WWWU" | recode -pf ..html)
+DATE=$(date +%s)
+echo "$NICK<br>"
+echo "$MSG<br>"
+echo "$WWW<br>"
+
+
+echo "$NICK|$DATE|$WWW|$MSG" >> "comments.txt"
+echo "<div><u>I wrote</u><br>"
+echo "<b>Nick:</b> $NICK<br>"
+echo "<b>Date:</b> "$(date --date="@$DATE" +"%T %D")"<br>"
+echo "<b>WWW:</b> $WWW<br>"
+echo "<b>Message:</b> <pre>$MSG</pre></div>"
+echo "<br>"
+echo "<a href=""comments.sh"">back</a>"
+echo "</body></html>"
+exit 0
